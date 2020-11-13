4d8b9ec9dcd7300374bb5b643f3fccf49635f86f
sangwookYun <brtm0533@naver.com>
2020-11-12 21:14:52 -0800

    2ndpushed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,101 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     YUNSANGWOOK
-    </div>
-  );
+
+import {Row, Col, Button} from 'react-bootstrap'
+import {Events, animateScroll as scroll, scroller } from 'react-scroll'
+import React, {Component} from 'react'
+import sample from './img/test2.mp4';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.scrollToTop = this.scrollToTop.bind(this);
+  } 
+  componentDidMount() {
+    Events.scrollEvent.register('begin', function() {
+      console.log("begin", arguments);
+    })
+    Events.scrollEvent.register('end', function() {
+      console.log("end", arguments);
+    })
+  } 
+  scrollToTop() {
+    scroll.scrollToTop();
+  }
+  scrollTo() {
+    scroller.scrollTo('scroll-to-element', {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart'
+    })
+  }
+  scrollToWithContainer() {
+
+    let goToContainer = new Promise((resolve, reject) => {
+
+      Events.scrollEvent.register('end', () => {
+        resolve();
+        Events.scrollEvent.remove('end');
+      });
+
+      scroller.scrollTo('scroll-container', {
+        duration: 800,
+        delay: 0,
+        smooth: 'easeInOutQuart'
+      });
+
+    });
+
+    goToContainer.then(() =>
+      scroller.scrollTo('scroll-container-second-element', {
+        duration: 800,
+        delay: 0,
+        smooth: 'easeInOutQuart',
+        containerId: 'scroll-container'
+      }));
+  }
+  componentWillUnmount() {
+    Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');
+  }
+
+   
+  render() {
+    return (
+      <div>
+        
+        
+        <div>
+          
+        {/* <NavBar></NavBar> */}
+          <Row className = "title">
+            <Col xl ={{span:6, offset:3}} style={{marginBottom:"20px"}}> 
+              <h2> Hello, My name is Sangwook Yun</h2>
+            </Col>
+            <Col xl ={{span:6, offset:3}} style={{marginBottom:"20px"}}>
+              <h4> I'm a Software Engineer</h4>
+            </Col>
+            <Col xl ={{span:4, offset:4}} style={{marginBottom:"20px"}}>
+              <Button className = "btn_primary" onClick = {()=>scroll.scrollTo(1300)}> About Me</Button>
+            </Col>
+       
+          </Row>
+          <video className='videoTag' autoPlay muted>
+            <source src={sample} type='video/mp4' />
+          </video>
+{/*           
+        <About></About>
+        <Project></Project>
+        <Footer></Footer>
+         */}
+        </div>
+      </div>
+      
+    );
+  }
 }
+  
 
 export default App;
